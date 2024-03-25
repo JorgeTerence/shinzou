bb155dac1bbbc0cef23d97a9156e29aaeb52eed6
Jorge Terence <jorge.terence@outlook.com>
2024-03-25 16:22:52 -0300

    Identified directives

--- src/ias.rs
+++ src/ias.rs
@@ -1,7 +1,9 @@
+use core::fmt;
+
 use crate::quit;
 
-#[derive(Clone, Copy)]
-enum Directive {
+#[derive(Clone, Copy, Debug)]
+pub enum Directive {
     Org,
     Set,
     Word,
@@ -12,17 +14,30 @@ enum Directive {
 impl Directive {
     fn new(call: &str) -> Self {
         match call {
-            "org" => Self::Org,
-            "set" => Self::Set,
-            "word" => Self::Word,
-            "align" => Self::Align,
-            "wfill" => Self::WFill,
+            ".org" => Self::Org,
+            ".set" => Self::Set,
+            ".word" => Self::Word,
+            ".align" => Self::Align,
+            ".wfill" => Self::WFill,
             _ => quit(&format!("Diretiva '{}' não reconhecida.", call), 1),
         }
     }
 }
 
-#[derive(Clone, Copy)]
+impl fmt::Display for Directive {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let label = match self {
+            Self::Org => ".org",
+            Self::Set => ".org",
+            Self::Word => ".org",
+            Self::Align => ".org",
+            Self::WFill => ".org",
+        };
+        write!(f, "{}", label)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 enum Operator {
     LoadFromMQ,     // LOAD MQ := AC <- MQ
     LoadMQ,         // LOAD MQ,M(X) := MQ <- M(X)
@@ -46,47 +61,104 @@ enum Operator {
     StoreRight,     // STOR M(X,28:39) := right of M(X) <- right of AC
 }
 
-// For the operations LSH, RSH and LOAD MQ, arg will be 0x00
+impl fmt::Display for Operator {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let label = match self {
+            Self::LoadFromMQ => "LOAD MQ",
+            Self::LoadMQ => "LOAD MQ,M(X)",
+            Self::LoadFromMemory => "LOAD M(X)",
+            Self::LoadNeg => "LOAD -M(X)",
+            Self::LoadAbs => "LOAD |M(X)|",
+            Self::JumpLeft => "JUMP M(X,0:19)",
+            Self::JumpRight => "JUMP M(X,20:39)",
+            Self::JumpLeftIf => "JUMP+M(X,0:19)",
+            Self::JumpRightIf => "JUMP+M(X,20:39)",
+            Self::Add => "ADD M(X)",
+            Self::AddAbs => "ADD |M(X)|",
+            Self::Sub => "SUB M(X)",
+            Self::SubAbs => "SUB |M(X)|",
+            Self::Mul => "MUL M(X)",
+            Self::Div => "DIV M(X)",
+            Self::Double => "LSH",
+            Self::Halve => "RSH",
+            Self::Store => "STOR M(X)",
+            Self::StoreLeft => "STOR M(X,8:19)",
+            Self::StoreRight => "STOR M(X,28:39)",
+        };
 
-union Command {
-    dir: Directive,
-    op: Operator,
+        write!(f, "{}", label)
+    }
 }
 
-union Argument {
-    addr: u16,
-    label: std::mem::ManuallyDrop<String>,
+// For the operations LSH, RSH and LOAD MQ, arg will be 0x00
+
+#[derive(Debug)]
+pub enum Command {
+    Directive(Directive),
+    Operator(Operator),
+    Label(String),
+}
+
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let label = match self {
+            Self::Directive(dir) => dir.to_string(),
+            Self::Operator(op) => op.to_string(),
+            Self::Label(s) => s.to_string(),
+        };
+        write!(f, "{}", label)
+    }
+}
+
+#[derive(Debug)]
+pub enum Argument {
+    Addr(u16),
+    Label(String),
+}
+
+impl fmt::Display for Argument {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let label = match self {
+            Self::Addr(n) => n.to_string(),
+            Self::Label(l) => l.to_string()
+        };
+        write!(f, "{}", label)
+    }
 }
 
 pub struct Instruction {
-    call: Command, // 8 bits
-    arg: Argument, // 12 bits
+    pub call: Command, // 8 bits
+    pub arg: Argument, // 12 bits
 }
 
 impl Instruction {
     pub fn new(line: &str) -> Self {
         // Directive
         if line.starts_with(".") {
-            let (call, arg) =  match line.split_once(' ') {
-                Some((s1, s2)) => (Command { dir: Directive::new(s1) }, match  s2.parse::<u16>() {
-                    Ok(n) => Argument {  },
-                    Err(e) => quit(&format!("Erro na linha '{}'. Argumento acima de 12 bits. O valor máximo é {}", line, u16::pow(2, 16)), 1)
-                }),
-                None => quit(&format!("Diretiva mal formatada: {}", line), 1)
+            match line.split_once(' ') {
+                Some((s1, s2)) => {
+                    let call = Command::Directive(Directive::new(s1));
+                    let arg = match s2.parse::<u16>() {
+                        Ok(n) => Argument::Addr(n),
+                        Err(e) => Argument::Label(s2.to_string()),
+                    };
+                    return Self { call, arg };
+                }
+                None => quit(&format!("Diretiva mal formatada: {}", line), 1),
             };
-
-            return Self { call, arg };
         }
         // Rótulos
         else if line.ends_with(":") {
-            Self {}
+            Self {
+                call: Command::Operator(Operator::Add),
+                arg: Argument::Addr(0x011),
+            }
         }
         // Operation
         else {
             Self {
-                call: Command {
-                    op: Operator::LoadFromMemory,
-                },
+                call: Command::Label("Hello world".to_string()),
+                arg: Argument::Addr(0x011),
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,36 @@ mod cli;
 mod ias;
 
 use cli::Args;
-use ias::Instruction;
+use ias::{Command, Instruction};
 use std::fs;
 use std::process;
 
 fn main() {
     let args = Args::new();
 
-    let program: Vec<Instruction> = vec![];
+    let mut program: Vec<Instruction> = vec![];
 
     match fs::read_to_string(args.asm_path) {
-        Ok(program) => {
-            for line in program.lines() {
+        Ok(code) => {
+            for line in code.lines() {
                 // Remove comments
                 let (line, _) = line.split_once("--").unwrap_or((line, ""));
 
-                let instruc = Instruction::new(line);
+                program.push(Instruction::new(line));
                 // TODO: binary code
             }
         }
         Err(_) => quit("Erro ao ler arquivo do programa.", 1),
     }
+
+    println!(
+        "{:?}",
+        program
+            .iter()
+            .filter(|i| matches!(i.call, Command::Directive(_)))
+            .map(|i| format!("{} {}", i.call, i.arg))
+            .collect::<Vec<String>>()
+    );
 }
 
 fn quit(msg: &str, code: i32) -> ! {
